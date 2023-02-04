562c3c48c857360beba24da0f422f3688165741c
Aman Srivastava <AmanRaj0100@users.noreply.github.com>
2023-02-04 21:50:32 +0530

    Update GitHub.java

--- GitHub.java
+++ GitHub.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         
-        System.out.println("Hello to GitHub");
+        System.out.println("Hello to CarryMinati");
+        System.out.println("To kaise hai aap log!!");
     }
 }
