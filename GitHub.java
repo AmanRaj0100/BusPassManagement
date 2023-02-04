4e1fbe0e130c016a3124ca6b9b68c6faf48ddaba
Aman Srivastava <AmanRaj0100@users.noreply.github.com>
2023-02-04 21:52:16 +0530

    Delete GitHub.java

--- GitHub.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        
-        System.out.println("Hello to CarryMinati");
-        System.out.println("To kaise hai aap log!!");
-    }
-}
